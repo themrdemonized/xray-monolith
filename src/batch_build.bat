aab48120b76573c68bd5927fe633be55c4e07755
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-08-08 15:09:49 +1000

    batch build bat to mitigate vs2022 broken batch build https://developercommunity.visualstudio.com/t/batch-build-broken-in-vs-2022/1671892 https://developercommunity.visualstudio.com/t/build-issues-with-multiple-configuration/1654460
    (cherry picked from commit 2f8ddd96664ab8aa83e6264ac4798842ecb94b87)

---
+++ src/batch_build.bat
@@ -0,0 +1,15 @@
+REM vswhere is required, install it via `choco install vswhere`
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  "%%i" engine-vs2022.sln /p:Configuration=DX11
+  "%%i" engine-vs2022.sln /p:Configuration=DX10
+  "%%i" engine-vs2022.sln /p:Configuration=DX9
+  "%%i" engine-vs2022.sln /p:Configuration=DX8
+  "%%i" engine-vs2022.sln /p:Configuration=DX11-AVX
+  "%%i" engine-vs2022.sln /p:Configuration=DX10-AVX
+  "%%i" engine-vs2022.sln /p:Configuration=DX9-AVX
+  "%%i" engine-vs2022.sln /p:Configuration=DX8-AVX
+  exit /b !errorlevel!
+)
