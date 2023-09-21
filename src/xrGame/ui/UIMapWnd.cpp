57cf7966181d53465bfd0451f0eadf333e91ec40
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-09-22 03:39:15 +1000

    map spot click fix, getting 4 from lvid, cleanup

--- src/xrGame/ui/UIMapWnd.cpp
+++ src/xrGame/ui/UIMapWnd.cpp
@@ -548,7 +548,7 @@ Fvector2 CUIMapWnd::GetGlobalMapCoordsForMouse()
 
 	// 1. Get cursor position in map space
 	// Normalize mouse coordinates in map canvas
-	Fvector2 pos_abs;
+	Fvector2 pos_abs = { 0, 0 };
 	auto cursor_pos = GetUICursor().GetCursorPosition();
 	cursor_pos.sub(ActiveMapRect().lt);
 
@@ -556,7 +556,7 @@ Fvector2 CUIMapWnd::GetGlobalMapCoordsForMouse()
 	cursor_pos.mul(-1);
 
 	// Get absolute left top of the current area of the map
-	Fvector2 map_abs;
+	Fvector2 map_abs = { 0, 0 };
 	Fvector2& current_zoom = gm->GetCurrentZoom();
 	gm->GetAbsolutePos(map_abs);
 	map_abs.sub(gm->WorkingArea().lt);
@@ -572,9 +572,10 @@ void CUIMapWnd::ActivatePropertiesBox(CUIWindow* w)
 {
 	m_UIPropertiesBox->RemoveAll();
 	luabind::functor<void> funct;
+	CMapSpot* sp = nullptr;
 	if (ai().script_engine().functor("pda.property_box_add_properties", funct))
 	{
-		CMapSpot* sp = smart_cast<CMapSpot*>(w);
+		sp = smart_cast<CMapSpot*>(w);
 		if (sp)
 			funct(m_UIPropertiesBox, sp->MapLocation()->ObjectID(), (LPCSTR)sp->MapLocation()->GetLevelName().c_str(),
 			      (LPCSTR)sp->MapLocation()->GetHint());
@@ -658,24 +659,87 @@ void CUIMapWnd::ActivatePropertiesBox(CUIWindow* w)
 			lm_real_mouse_pos.y = lm_bound_rect.height() + lm_bound_rect.lt.y - lm_adjusted_mouse_pos.y / lm_zoom.x;
 			lm_real_mouse_pos.x /= UI().get_current_kx();
 
-			// Get Y component of real world coordinates - raycast
-
 			// Get map name
 			LPCSTR lm_name = lm->MapName().c_str();
 
+			// Get Y component of real world coordinates - raycast. Only on current level
+			// On other levels y will be taken from lvid
+			// Get accurate lvid as well
+			Fvector lm_real_pos = { lm_real_mouse_pos.x, 0, lm_real_mouse_pos.y };
+			u32 lm_lvid = u32(-1);
+			if (0 == xr_strcmp(g_pGameLevel->name().c_str(), lm_name)) {
+
+				// perform ray cast to get actual position
+				collide::rq_result R;
+				CObject* ignore = Actor();
+				Fbox lm_bv = g_pGameLevel->ObjectSpace.GetBoundingVolume();
+				Fvector start = { lm_real_mouse_pos.x, lm_bv.max.y, lm_real_mouse_pos.y };
+				Fvector dir = { 0, -1, 0 };
+				float range = lm_bv.max.y - lm_bv.min.y;
+				Fvector res;
+				if (Level().ObjectSpace.RayPick(start, dir, range, collide::rqtStatic, R, ignore))
+				{
+					res.mad(start, dir, R.range);
+					lm_real_pos.y = res.y;
+					lm_lvid = ai().level_graph().vertex_id(lm_real_pos);
+				}
+			}
+
+			bool lvid_not_set = lm_lvid == u32(-1);
+			u16 lm_gvid = u16(-1);
+
+			// Get gvid and set lvid if wasnt set
+			auto& gg = ai().game_graph();
+			u32 current_gvid = 0;
+			float dist = FLT_MAX;
+			while (gg.valid_vertex_id(current_gvid)) {
+				auto vertex = gg.vertex(current_gvid);
+				if (!vertex) {
+					current_gvid++;
+					continue;
+				}
+				GameGraph::_LEVEL_ID level_id = vertex->level_id();
+				const GameGraph::LEVEL_MAP& levels = ai().game_graph().header().levels();
+				GameGraph::LEVEL_MAP::const_iterator I = levels.find(level_id);
+				if (I == levels.end()) {
+					current_gvid++;
+					continue;
+				}
+				LPCSTR level_name = I->second.name().c_str();
+				if (0 == xr_strcmp(level_name, lm_name)) {
+					Fvector3 pos = vertex->level_point();
+					float d = lm_real_pos.distance_to_xz_sqr(pos);
+					if (d < dist) {
+						dist = d;
+						lm_gvid = current_gvid;
+						if (lvid_not_set) {
+							lm_lvid = vertex->level_vertex_id();
+							Fvector3 lvid_pos = ai().level_graph().vertex_position(lm_lvid);
+							lm_real_pos.y = lvid_pos.y;
+						}
+					}
+				}
+				current_gvid++;
+			}
+
 			luabind::object table = luabind::newtable(ai().script_engine().lua());
-			table["gm_cursor_pos"] = cursor_pos;
+			/*table["gm_cursor_pos"] = cursor_pos;
 			table["gm_map_abs"] = map_abs;
-			table["gm_pos_abs"] = pos_abs;
+			table["lm_bound_rect"] = lm_bound_rect;
+			table["lm_bound_size"] = lm_bound_size;
 			table["gm_zoom"] = current_zoom;
 			table["lm_rect"] = lm_rect;
 			table["lm_zoom"] = lm_zoom;
 			table["lm_mouse_pos"] = lm_mouse_pos;
 			table["lm_mouse_pos_adjusted"] = lm_adjusted_mouse_pos;
-			table["lm_mouse_pos_real"] = lm_real_mouse_pos;
-			table["lm_bound_rect"] = lm_bound_rect;
-			table["lm_bound_size"] = lm_bound_size;
-			table["lm_name"] = lm_name;
+			table["lm_mouse_pos_real"] = lm_real_mouse_pos;*/
+			table["global_map_pos"] = pos_abs;
+			table["pos"] = lm_real_pos;
+			table["level_name"] = lm_name;
+			table["object_id"] = sp ? sp->MapLocation()->ObjectID() : 65535;
+			table["hint"] = sp ? sp->MapLocation()->GetHint() : NULL;
+			table["lvid"] = lm_lvid;
+			table["gvid"] = lm_gvid;
 
 			// 3. Lua
 			rcFunct(m_UIPropertiesBox, table);
