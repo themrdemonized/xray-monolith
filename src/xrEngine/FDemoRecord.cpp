f3a5fa0da080e59a85548ddb27a8af67defee2d7
Dmitry Chernyavsky <46323442+themrdemonized@users.noreply.github.com>
2024-04-07 14:36:10 +1000

    Merge pull request #37 from strangerism/all-in-one-vs2022-wpo
    DemoRecord Update - Cursor visibility toggle, F12 keypress, camera boundary check
    
    (cherry picked from commit 67c030dbb1d307adecbb2ccf62ca1afa1fdc734e)

--- src/xrEngine/FDemoRecord.cpp
+++ src/xrEngine/FDemoRecord.cpp
@@ -10,7 +10,7 @@
 #include "render.h"
 #include "CustomHUD.h"
 #include "CameraManager.h"
-
+#include "../xrGame/UICursor.h"
 extern BOOL g_bDisableRedText;
 static Flags32 s_hud_flag = {0};
 static Flags32 s_dev_flags = {0};
@@ -85,6 +85,7 @@ CDemoRecord::CDemoRecord(const char* name, float life_time) : CEffectorCam(cefDe
 		g_position.set_position = false;
 		IR_Capture(); // capture input
 		m_Camera.invert(Device.mView);
+		m_fCameraBoundary = 100.f;
 
 		// parse yaw
 		Fvector& dir = m_Camera.k;
@@ -100,7 +101,8 @@ CDemoRecord::CDemoRecord(const char* name, float life_time) : CEffectorCam(cefDe
 		m_HPB.z = 0;
 
 		m_Position.set(m_Camera.c);
-
+		m_Actor_Position.set(m_Camera.c.x,m_Camera.c.y,m_Camera.c.z);
+		m_fGroundPosition = m_Actor_Position.y - 1.0f;
 		m_vVelocity.set(0, 0, 0);
 		m_vAngularVelocity.set(0, 0, 0);
 		iCount = 0;
@@ -111,7 +113,7 @@ CDemoRecord::CDemoRecord(const char* name, float life_time) : CEffectorCam(cefDe
 		m_bMakeScreenshot = FALSE;
 		m_bMakeLevelMap = FALSE;
 		return_ctrl_inputs = FALSE;
-
+		m_CameraBoundaryEnabled = FALSE;
 		m_fSpeed0 = pSettings->r_float("demo_record", "speed0");
 		m_fSpeed1 = pSettings->r_float("demo_record", "speed1");
 		m_fSpeed2 = pSettings->r_float("demo_record", "speed2");
@@ -135,6 +137,8 @@ CDemoRecord::CDemoRecord(const char* name, xr_unordered_set<CDemoRecord*>* pDemo
 	if (!file) {
 		StopDemo();
 	}
+	// when demo_record is launched, mouse controls are redirected to camera movements, so we hide the cursor
+	GetUICursor().Hide();
 }
 
 void CDemoRecord::StopDemo() {
@@ -147,7 +151,10 @@ void CDemoRecord::StopDemo() {
 void CDemoRecord::EnableReturnCtrlInputs() {
 	return_ctrl_inputs = TRUE;
 }
-
+void CDemoRecord::SetCameraBoundary(float boundary) {
+	m_CameraBoundaryEnabled = TRUE;
+	m_fCameraBoundary = boundary;
+}
 CDemoRecord::~CDemoRecord()
 {
 	if (file)
@@ -320,6 +327,8 @@ BOOL CDemoRecord::ProcessCam(SCamEffectorInfo& info)
 	info.dont_apply = false;
 	if (0 == file) return TRUE;
 
+	m_Starting_Position.set(m_Position.x, m_Position.y, m_Position.z);
+
 	if (m_bMakeScreenshot)
 	{
 		MakeScreenshotFace();
@@ -425,9 +434,33 @@ BOOL CDemoRecord::ProcessCam(SCamEffectorInfo& info)
 		vmove.mul(m_vT.y);
 		m_Position.add(vmove);
 
+		if (m_CameraBoundaryEnabled)
+		{
+			float x = m_Position.x;
+			float y = m_Position.y;
+			float z = m_Position.z;
+			// let's check if we're out of bound
+			if (_abs(m_Position.x - m_Actor_Position.x) > m_fCameraBoundary)
+			{
+				x = m_Starting_Position.x;
+			}
+			if (_abs(m_Position.y - m_Actor_Position.y) > m_fCameraBoundary)
+			{
+				y = m_Starting_Position.y;
+			}
+			if (_abs(m_Position.z - m_Actor_Position.z) > m_fCameraBoundary)
+			{
+				z = m_Starting_Position.z;
+			}
+			// fake groud collision check
+			if (m_Position.y < m_fGroundPosition){
+				y = m_Starting_Position.y;
+			}
+			m_Position.set(x, y, z);
+		}
+
 		m_Camera.setHPB(m_HPB.x, m_HPB.y, m_HPB.z);
 		m_Camera.translate_over(m_Position);
-
 		// update camera
 		info.n.set(m_Camera.j);
 		info.d.set(m_Camera.k);
@@ -472,9 +505,18 @@ void CDemoRecord::IR_OnKeyboardPress(int dik)
 
 		if (m_b_redirect_input_to_level)
 		{
+			// control inputs are redirected to the invoker
+			if (return_ctrl_inputs){
+				// if the invoker is a script that launched demo_record with return_ctrl_inputs enabled we show the cursor
+				GetUICursor().Show();
+			}
 			g_pGameLevel->IR_OnKeyboardPress(dik);
 			return;
 		}else{
+			if (GetUICursor().IsVisible()){
+				// mouse controls are back to the camera movements, so we hide the cursor
+				GetUICursor().Hide();
+			}
 			// we end up here if controls have not been redirected to the invoker
 			// if we launched demo_record_return_ctrl_inputs we return the event DIK_TAB key press also to the invoker 
 			if (dik == DIK_TAB && return_ctrl_inputs) {
@@ -495,6 +537,8 @@ void CDemoRecord::IR_OnKeyboardPress(int dik)
 			StopDemo();
 			// if we launched demo_record_return_ctrl_inputs we return the event ESCAPE key press also to the launcher entity
 			if (return_ctrl_inputs){
+				// we also show the cursor
+				GetUICursor().Show();
 				// sends upstream the DIK_ESCAPE keyboard press event. This key quit demo_record and returns controls to its launcher (game console or script)
 				// This can help scripts that execute command demo_record_photomode to know when the demo_record has exited
 				g_pGameLevel->IR_OnKeyboardPress(dik);
@@ -525,8 +569,14 @@ void CDemoRecord::IR_OnKeyboardPress(int dik)
 void CDemoRecord::IR_OnKeyboardRelease(int dik)
 {
 	if (isInputBlocked) return;
-	if (m_b_redirect_input_to_level)
+	if (m_b_redirect_input_to_level){
 		g_pGameLevel->IR_OnKeyboardRelease(dik);
+	}else{
+		if (dik == DIK_F12 && return_ctrl_inputs) {
+			// if demo_record_return_ctrl_inputs is enabled we return the event F12 key release also to the launcher entity
+			g_pGameLevel->IR_OnKeyboardRelease(dik);
+		}		
+	}
 }
 
 static void update_whith_timescale(Fvector& v, const Fvector& v_delta)
