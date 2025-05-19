83da17abd8c7a7b76ef689111a8653b0b0e254c1
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-19 19:57:47 +0100

    Expose `CScriptDialect` to luabind

--- src/xrServerEntities/script_dialect.h
+++ src/xrServerEntities/script_dialect.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#include "script_export_space.h"
 #include <string>
 
 typedef std::set<std::string> Unlocalizer;
 typedef xr_unordered_map<std::string, Unlocalizer> Unlocalizers;
 
-class CScriptDialect
+class CScriptDialect : public DLL_Pure
 {
 public:
-    virtual bool recognize(const std::string& src, LPCSTR caNameSpaceName) const = 0;
+    virtual bool recognize(const std::string& src, LPCSTR caNameSpaceName) const;
     virtual std::string unlocalize(Unlocalizer& unlocalizer, const std::string& src, LPCSTR caNameSpaceName) const;
     virtual std::string lift(const std::string& src, LPCSTR caNameSpaceName) const;
+
+DECLARE_SCRIPT_REGISTER_FUNCTION
 };
+
+add_to_type_list(CScriptDialect)
+#undef script_type_list
+#define script_type_list save_type_list(CScriptDialect)
