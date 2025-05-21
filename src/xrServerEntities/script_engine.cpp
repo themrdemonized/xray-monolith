a7b0877383e0f9b9041f72c20ffae6524251172a
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-21 22:30:06 +0100

    Move script compiler internals to Lua

--- src/xrServerEntities/script_engine.cpp
+++ src/xrServerEntities/script_engine.cpp
@@ -400,7 +400,6 @@ void CScriptEngine::init()
 #endif // #ifndef USE_LUA_STUDIO
 	//	lua_sethook							(lua(), lua_hook_call,	LUA_MASKLINE|LUA_MASKCALL|LUA_MASKRET,	0);
 
-	ScriptCompiler().load_unlocalizers();
 	process_file_if_exists("_init", false);
 	process_file_if_exists("_G", false);
 
