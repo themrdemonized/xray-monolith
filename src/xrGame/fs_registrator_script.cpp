47bf2ae6fc35a48ffe683085122864224f71f136
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-25 15:20:19 +0100

    Respect `FS_RootOnly` in modified `FS_file_list_ex`

--- src/xrGame/fs_registrator_script.cpp
+++ src/xrGame/fs_registrator_script.cpp
@@ -131,9 +131,9 @@ public:
 FS_file_list_ex::FS_file_list_ex(LPCSTR path, u32 flags, LPCSTR mask)
 {
 	FS_Path* P = FS.get_path(path);
-	P->m_Flags.set(FS_Path::flRecurse, TRUE);
+	P->m_Flags.set(FS_Path::flRecurse, !(flags & FS_RootOnly));
 	P->m_Flags.set(FS_Path::flNeedRescan,TRUE);
-	FS.m_Flags.set(CLocatorAPI::flNeedCheck,FALSE);
+	FS.m_Flags.set(CLocatorAPI::flNeedCheck, flags & FS_RootOnly);
 	FS.rescan_pathes();
 
 	FS_FileSet files;
