a4965b653ebc0f46e9cd2ba1c5383c9660b49b68
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-25 07:08:11 +0100

    Revert "Integrate Fennel 1.5.3 with `lua-extensions`"
    This reverts commit 5b29cc6db566b273c0e373afa5ccc89e4e78d475.

--- src/3rd party/lua-extensions/base.cpp
+++ src/3rd party/lua-extensions/base.cpp
@@ -16,8 +16,6 @@ extern "C"{
     #include "lmarshal.h"
 }
 
-#include "lfennel.h"
-
 //#include "Libs.h"
 #include "script_additional_libs.h"
 
@@ -39,7 +37,6 @@ int luaopen_lua_extensions(lua_State *L){
     luaopen_marshal(L);
     //open_kb(L);
     //open_log(L); 
-    luaopen_fennel(L);
 
 	luaL_register(L, "lua_extensions", R);
 	return 0;
--- src/3rd party/lua-extensions/lfennel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "lua.hpp"
-
-typedef unsigned long DWORD;
-typedef unsigned char BYTE;
-
-int luaopen_fennel (lua_State *L);
