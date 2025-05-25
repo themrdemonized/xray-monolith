729fd0eb893ac0e39917c689f3337079091abe94
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-25 06:59:02 +0100

    Extend `FS_file_list_ex` script interface to load recursively by default
    - Allows `axr_main`, MCM, and other such systems to support nested files

--- src/xrGame/fs_registrator_script.cpp
+++ src/xrGame/fs_registrator_script.cpp
@@ -131,8 +131,9 @@ public:
 FS_file_list_ex::FS_file_list_ex(LPCSTR path, u32 flags, LPCSTR mask)
 {
 	FS_Path* P = FS.get_path(path);
+	P->m_Flags.set(FS_Path::flRecurse, TRUE);
 	P->m_Flags.set(FS_Path::flNeedRescan,TRUE);
-	FS.m_Flags.set(CLocatorAPI::flNeedCheck,TRUE);
+	FS.m_Flags.set(CLocatorAPI::flNeedCheck,FALSE);
 	FS.rescan_pathes();
 
 	FS_FileSet files;
