941158afb0c2f5ddb2c4ea28d277e6e660264877
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-26 03:05:52 +0100

    Proper error handling for `macro/lua`

--- gamedata/scripts/macro/lua/init.lua
+++ gamedata/scripts/macro/lua/init.lua
@@ -1,12 +1,37 @@
 local function expand(src, namespace_name)
    print("* lua: expanding", namespace_name)
-   local macro = require("macro")
-   return setfenv(
-      macro.load_src(src, namespace_name),
-      macro.extend_env {
-         _PACKAGE = namespace_name
-      }
+   print(src)
+
+   local res, mod = pcall(loadstring, src, namespace_name)
+   if not res then
+      local msg = "! lua: error loading " .. namespace_name .. ":\n\n"
+               .. mod .. "\n"
+      print(msg)
+      error(msg)
+   end
+
+   local mac = setfenv(
+      mod,
+      setmetatable(
+         { _PACKAGE = namespace_name },
+         {
+            __index = _G,
+            __newindex = _G,
+         }
+      )
    )
+
+   return function()
+      local evaluated, out = pcall(mac)
+      if not evaluated then
+         out = "! lua: error evaluating " .. namespace_name .. ":\n\n"
+            .. out .. "\n"
+         print(out)
+         error(out)
+      end
+
+      return out
+   end
 end
 
 require("scam/compiler").register_extension("lua", expand)
