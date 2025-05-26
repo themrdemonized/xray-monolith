9f1e99ccc4eab9e362309e54737b3736838f2615
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-26 02:30:37 +0100

    Pass `namespace_name` to `load_src` in `macro/lua`, fix nil concat

--- gamedata/scripts/macro/lua/init.lua
+++ gamedata/scripts/macro/lua/init.lua
@@ -1,8 +1,8 @@
 local function expand(src, namespace_name)
-   print("* lua: expanding " .. namespace_name)
+   print("* lua: expanding", namespace_name)
    local macro = require("macro")
    return setfenv(
-      macro.load_src(src),
+      macro.load_src(src, namespace_name),
       macro.extend_env {
          _PACKAGE = namespace_name
       }
