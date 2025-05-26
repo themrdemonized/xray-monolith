e98b8857fb8d2a3ff579578e3665debddcceb10b
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-26 05:28:04 +0100

    Simplify `macro` module

--- gamedata/scripts/macro/init.lua
+++ gamedata/scripts/macro/init.lua
@@ -1,22 +1,5 @@
-local function load_src(src, script_name)
-   return assert(loadstring(src, script_name))
-end
-
-local function extend_env(dest)
-   return setmetatable(
-      dest,
-      {
-         __index = _G,
-         __newindex = _G,
-      }
-   )
-end
-
-package.loaded[_PACKAGE] = {
-   load_src = load_src,
-   extend_env = extend_env,
+return {
+   lua  = require(_PACKAGE .. "/lua"),
+   lisp = require(_PACKAGE .. "/lisp"),
+   wua = require(_PACKAGE .. "/wua"),
 }
-
-package.loaded[_PACKAGE].lua = require(_PACKAGE .. "/lua")
-package.loaded[_PACKAGE].lisp = require(_PACKAGE .. "/lisp")
-package.loaded[_PACKAGE].wua = require(_PACKAGE .. "/wua")
