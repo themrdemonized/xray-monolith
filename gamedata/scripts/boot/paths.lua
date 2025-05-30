d751e1e6ef33b72a03c1ebd9c29fbfef71e75a22
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-30 22:28:59 +0100

    Clear `package.path` when first compiler extension is registered

--- gamedata/scripts/boot/paths.lua
+++ gamedata/scripts/boot/paths.lua
@@ -1,10 +1,20 @@
 _PACKAGE = "boot/paths"
 
+local first_run = true
+
 -- Define load path registrator
 function _REGISTER_PATHS(...)
+   -- If this is the first registered path, clear existing paths
+   if first_run then
+      first_run = false
+      package.path = ""
+   end
+
    local ps = {...}
    for i=#ps,1,-1 do
       p = getFS():update_path("$game_scripts$", ps[i])
       package.path = p .. ";" .. package.path
    end
+
+   print("package.path: " .. package.path)
 end
