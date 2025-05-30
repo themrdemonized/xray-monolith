e7e8d06ae970eafa1c7e46e71ab3215aae08b611
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-05-30 22:28:44 +0100

    Clear C import path at boot time

--- gamedata/scripts/init.lua
+++ gamedata/scripts/init.lua
@@ -8,6 +8,9 @@ local fs = getFS()
 local base = fs:update_path("$game_scripts$", "")
 package.path = base .. "?.lua;" .. base .. "?/init.lua"
 
+--- Clear the C import path
+package.cpath = ""
+
 --- Initialize environment via the boot module
 require("boot")
 
