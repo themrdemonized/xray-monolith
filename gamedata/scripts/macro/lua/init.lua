82b3076366e74a70a12108be9b2f391212498827
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-06-03 22:01:50 +0100

    Reintegrate latest `macro/wua`

--- gamedata/scripts/macro/lua/init.lua
+++ gamedata/scripts/macro/lua/init.lua
@@ -1,11 +1,55 @@
-function expand(src, namespace_name)
-   print("lua: expanding " .. namespace_name)
-   return setfenv(
-      macro.load_src(src),
-      macro.extend_env {
-         script_name = function()
-            return namespace_name
-         end
+local function handle_error(msg, namespace_name)
+   return function(err)
+      package.loaded[namespace_name] = nil
+      err = "! " .. _PACKAGE .. ": "
+         .. msg .. ":\n\n"
+         .. debug.traceback(err .. "\n", 2)
+         .. "\n"
+      print(err)
+      error(err)
+   end
+end
+
+local function expand(src, namespace_name, script_name)
+   print("* " .. _PACKAGE .. ": expanding", namespace_name)
+
+   local mod, err = loadstring(src, namespace_name)
+   if not mod then
+      handle_error(
+         "error loading " .. namespace_name,
+         namespace_name
+   )(err)
+   end
+
+   local env = setmetatable(
+      {
+         _PACKAGE = namespace_name,
+         _FILE = script_name,
+      },
+      {
+         __index = _G,
+         __newindex = _G,
       }
    )
+
+   local mac = setfenv(mod, env)
+
+   return function()
+      package.loaded[namespace_name] = env
+      local _, out = xpcall(
+         mac,
+         handle_error(
+            "error evaluating " .. namespace_name,
+            namespace_name
+         )
+      )
+
+      return out
+   end
 end
+
+require("scam/compiler").register_extension("lua", expand)
+
+return {
+   expand = expand
+}
