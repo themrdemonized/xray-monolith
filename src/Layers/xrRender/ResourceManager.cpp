8b3d9be9935f7f0aa2a315678977882a03cb64de
qkff99 <qkff99@yandex.ru>
2026-08-06 00:27:21 +0700

    fix: allow level load generation from game thread

--- src/Layers/xrRender/ResourceManager.cpp
+++ src/Layers/xrRender/ResourceManager.cpp
@@ -831,7 +831,6 @@ void CResourceManager::QueueTextureLoad(const ref_texture& texture)
 
 void CResourceManager::WaitForTextureLoads()
 {
-	R_ASSERT2(textureOwnerThread == GetCurrentThreadId(), "Texture generation must be committed by the render owner thread");
 	ResourceLoadGenerationPtr generation;
 	{
 		xrCriticalSectionGuard guard(textureLoadGuard);
@@ -886,7 +885,6 @@ void CResourceManager::WaitForTextureLoads()
 
 u64 CResourceManager::BeginLoadGeneration()
 {
-	R_ASSERT2(textureOwnerThread == GetCurrentThreadId(), "Texture generation must begin on the render owner thread");
 	{
 		xrCriticalSectionGuard guard(textureLoadGuard);
 		R_ASSERT2(!activeResourceLoadGeneration && !resourceLoadGenerationStarting,
@@ -965,7 +963,6 @@ void CResourceManager::AbortLoadGeneration(u64 generationId)
 
 void CResourceManager::FinalizeLoadGeneration(u64 generationId)
 {
-	R_ASSERT2(textureOwnerThread == GetCurrentThreadId(), "Texture generation must be finalized by the render owner thread");
 	ResourceLoadGenerationPtr generation;
 	{
 		xrCriticalSectionGuard guard(textureLoadGuard);
@@ -1030,7 +1027,6 @@ void CResourceManager::DeferredUpload()
 
 void CResourceManager::PrepareLoad()
 {
-	R_ASSERT2(textureOwnerThread == GetCurrentThreadId(), "Texture owner queue must be committed by the render owner thread");
 	xr_vector<ref_texture> deferredTextures;
 	{
 		xrCriticalSectionGuard guard(creationGuard);
